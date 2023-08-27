96ebefa4febcd67535d6207e7cd2f8225969a5ce
OGaye <gayeomar@hotmail.com>
2023-08-27 13:43:49 -0400

    memcached

--- src/main/java/com/bofa/demo/demosessionexternalization/DemoSessionExternalizationApplication.java
+++ src/main/java/com/bofa/demo/demosessionexternalization/DemoSessionExternalizationApplication.java
@@ -2,12 +2,26 @@ package com.bofa.demo.demosessionexternalization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
+@EnableCaching
 public class DemoSessionExternalizationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoSessionExternalizationApplication.class, args);
+        ApplicationContext ctx =  SpringApplication.run(DemoSessionExternalizationApplication.class, args);
+        //displayAllBeans(ctx);
     }
 
+    public static void displayAllBeans(ApplicationContext ctx) {
+        List<String> beans = Arrays.asList(ctx.getBeanDefinitionNames());
+
+        beans.forEach((n) -> {
+            System.out.println("*** A bean: " + n);
+        });
+    }
 }
